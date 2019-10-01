0c22e86aed9756a56dd99f5600f9376b2ab91583
Sebuliba-Adrian <adrian.sebuliba1@gmail.com>
2019-10-01 06:22:00 +0300

    Initial setup with react router and page reload

---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <div>
+            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, hic! Vero, porro amet quas odit saepe dolore atque quam ea, similique commodi blanditiis eaque corrupti optio dolor nesciunt odio nihil. </p>
+        </div>
+    )
+}
