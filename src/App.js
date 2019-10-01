029720898058b6c34d2657f7aab980a21d955d8b
Sebuliba-Adrian <adrian.sebuliba1@gmail.com>
2019-10-01 09:57:42 +0300

    Add swithc tag to load up one component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import React from 'react';
-import NavBar from './components/NavBar';
-import {BrowserRouter as Router, Route } from 'react-router-dom';
-import './App.css';
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
-import Post from './components/Post';
+import React from "react";
+import NavBar from "./components/NavBar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Post from "./components/Post";
 
 function App() {
   return (
     <Router>
-    <div className="App">
-      <NavBar />
-      <Route exact path='/' component={Home} />
-      <Route path='/about' component={About} />
-      <Route path='/contact' component={Contact} />
-      <Route path='/:post_id' component={Post} />
-    </div>
+      <div className="App">
+        <NavBar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/:post_id" component={Post} />
+        </Switch>
+      </div>
     </Router>
   );
 }
--- src/components/Post.js
+++ src/components/Post.js
@@ -27,7 +27,7 @@ class Post extends Component {
     );
     return (
       <div className="container">
-        <h4>{post}</h4>
+        {post}
       </div>
     );
   }
