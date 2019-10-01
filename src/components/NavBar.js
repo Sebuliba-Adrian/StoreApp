97975c5821118fde6ab031af821f18e150912036
Sebuliba-Adrian <adrian.sebuliba1@gmail.com>
2019-10-01 06:35:16 +0300

    Prevent loading from server

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,14 @@
-import React from 'react'
+import React from 'react';
+import {Link, NavLink } from 'react-router-dom';
 
 export default function NavBar() {
     return (
 
             <nav>
                 <ul>
-                <li><a href='/'>Home</a></li>
-                <li><a href='/about'>About</a></li>
-                <li><a href='/contact'>Contact</a></li>
+                <li><Link to='/'>Home</Link></li>
+                <li><Link to='/about'>About</Link></li>
+                <li><Link to='/contact'>Contact</Link></li>
                 </ul>
             </nav>
         
