e967947b4a290243fff3a1b6a0ca2069786c9f22
Sebuliba-Adrian <adrian.sebuliba1@gmail.com>
2019-10-01 08:30:08 +0300

    Add ability to list all posts

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, {Component} from 'react'
+import Axios from 'axios'
 
-export default function Home() {
-    return (
-        <div>
-            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, hic! Vero, porro amet quas odit saepe dolore atque quam ea, similique commodi blanditiis eaque corrupti optio dolor nesciunt odio nihil. </p>
-        </div>
-    )
+class Home extends Component {
+    state ={
+        posts: [],
+    }
+    componentDidMount(){
+
+        Axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(res => {
+            console.log(res)
+            this.setState({
+                posts: res.data.slice(0,10)
+            });
+        })
+    }
+    render() {
+       const {posts} = this.state;
+       const postList = posts.length?(posts.map(post =><div key={post.id}><p>{post.body}</p></div>)
+        ) : (<div>No posts yet</div>);
+        
+        return (postList)
+    }
 }
+export default Home;
