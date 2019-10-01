58c1bcbc9b752d7fbbeb519583ddd365ae67bce0
Sebuliba-Adrian <adrian.sebuliba1@gmail.com>
2019-10-01 09:49:32 +0300

    Add styles and pulling out single post

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,11 +8,14 @@ import {Link, NavLink, withRouter } from 'react-router-dom';
     return (
 
             <nav>
-                <ul>
+            <div className="nav-wrapper">
+                <Link to="#" className="brand-logo">Logo</Link>
+                <ul className="right hide-on-med-and-down">
                 <li><Link to='/'>Home</Link></li>
                 <li><Link to='/about'>About</Link></li>
                 <li><Link to='/contact'>Contact</Link></li>
                 </ul>
+             </div>
             </nav>
         
     )
