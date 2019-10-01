81752034f5bae0ce3f32b61d87d4d327cf0380e7
Sebuliba-Adrian <adrian.sebuliba1@gmail.com>
2019-10-01 06:40:02 +0300

    Capture props provided by route

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
-export default function Contact() {
+export default function Contact(props) {
+    console.log(props);
     return (
         <div>
             <p>Contact page</p>
